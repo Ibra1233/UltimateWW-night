abffec0d89a0ba343b4af945ae62b9db60a95cf9
ibra1233 <ibrahim.akcura@hotmail.com>
2026-01-12 18:34:43 +0100

    Ændret kommentarer til JavaScript (ingen funktionelle ændringer)

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ const closeBtn = document.querySelector(".close"); /* Krydset i modalen */
 const overlay = document.querySelector(".modal-overlay"); /* Baggrund bag modal /*
 /* Alle ovenstående henter elementer fra HTML*/
 
-if (modal)                                      /* Hvis modalen findes, så JS ikke laver fejl på siden */ {
+if (modal)                                      /* Hvis modalen findes, vil koden køre indeni, men hvis ikke springes den over */ {
     openBtns.forEach(btn => {   /* Går igennem alle knapper med klasse .openForm (alle knapper åbner samme modal */
         btn.addEventListener("click", (e) => { /* Ved klik åbner modal */
-            e.preventDefault(); /* Stopepr browserens standardhandling, eksempelvis at tryk på knappen vil reloade siden */
-            modal.style.display = "block"; /* Viser popup */
+            e.preventDefault(); /* Stopper browserens standardhandling, eksempelvis at tryk på knappen vil reloade siden */
+            modal.style.display = "block"; /* Gør modalen synlig på siden */
         });
     });
 
-    closeBtn.addEventListener("click", () => {      /* Lukker modalen når der klikkes på kryds */
+    closeBtn.addEventListener("click", () => {      /* Når der klikkes på luk knappen */
         modal.style.display = "none";           /* Modalen skjules */
     });
 
-    overlay.addEventListener("click", () => { /*Lukker modalen når der klikkes udenfor modal vinduet
-        modal.style.display = "none"; /* Modalen skjules */
+    overlay.addEventListener("click", () => { /*Når der klikkes udenfor vinduet med åben modal */
+        modal.style.display = "none"; /* Modalen lukker */
     });
 }
 
@@ -32,7 +32,7 @@ const navLinks = document.querySelector(".nav-links"); /* Finder menuen med link
 
 if (burger && navLinks) { /* Tjekker hvis begge elementer findes, og forhindrer JavaScript-fejl, hvis noget mangler */
     burger.addEventListener("click", () => { /* JavaScript registrerer klikket */
-        navLinks.classList.toggle("active"); /* Koden går videre til menuen */
+        navLinks.classList.toggle("active"); /* Koden går videre til menuen eller lukker menuen */
         burger.classList.toggle("toggle"); /* Forberedt til visuel ændring/animation af burgerikonet via CSS */
     });
 }
